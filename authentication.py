a7eca35d9074e9108268977ba90f5245969123b8
clintonMF <mekwunyeclinton22@gmail.com>
2022-03-03 23:17:00 +0100

    first commit

---
+++ authentication.py
@@ -0,0 +1,89 @@
+filename = 'database.txt'
+def register():
+    """A function for the registration of user"""
+    db = open(filename,'r')
+    
+#    creating the database which is a dictionary containing the username as the key and passsword as the value
+    username = input('create username: ')
+    password = input('Create  password: ')
+    password1 = input('Confirm password: ')
+    lines = db.readlines()
+    usernames = []
+    passwords = []
+    for line in lines:
+        username_lst, password_lst = line.split(',')
+        username_lst = username_lst.strip()
+        password_lst = password_lst.strip()
+        usernames.append(username_lst)
+        passwords.append(password_lst)
+    profile_dict = dict(zip(usernames,passwords))
+
+    # the logic begind the registration
+    if username in usernames:
+        print('Username exist, create another username.') 
+        register()   
+    elif password1 != password:
+        print('password does not match, please restart')
+        register()
+    elif len(password) <= 6:
+        print('Password is too short, please create another.')
+        register()
+    else:
+        db=open(filename, 'a')
+        db.write(f" {username},{password} \n")
+        print('success')
+    db.close()
+
+def access():
+    """A function that enables the user to login into the application"""
+    db = open(filename,'r')
+    username = input('enter username: ')
+    password = input('enter password: ')
+    
+    if len(username)>1 and len(password) >1:
+        
+        lines = db.readlines()
+        usernames = []
+        passwords = []
+        for line in lines:
+            username_lst, password_lst = line.split(',')
+            username_lst = username_lst.strip()
+            password_lst = password_lst.strip()
+            usernames.append(username_lst)
+            passwords.append(password_lst)
+        profile_dict = dict(zip(usernames,passwords))
+        
+        # the logic behind the login
+        try:
+            if username in profile_dict[username]:
+                try:
+                    if password != profile_dict[username]:
+                        print('incorrect password or username')
+                        access()
+                    else:
+                        print('login success')
+                        print(f"Hi, {username}!")
+                except:
+                    print('incorrect password or username')
+        except:
+            print("User name does not exist")
+    else:
+        print('please enter a value')
+            
+def home(option = None):
+    """The home page function"""
+    option = input("Login | signup: ")
+    if option == "login":
+        access()
+    elif option == "signup":
+        register()
+    else:
+        print("please enter an option")
+        home()
+        
+home()
+    
+       
+    
+    
+    
